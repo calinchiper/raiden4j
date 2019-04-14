9d7036ebecac4139327bd6d381208178767b7608
Calin Chiper <CalinC@yonder.local>
2019-04-14 21:15:23 +0300

    changed groupId

--- src/main/java/http/RaidenService.java
+++ src/main/java/http/RaidenService.java
@@ -8,7 +8,6 @@ import dto.Channel;
 import dto.Payment;
 import dto.PaymentResponse;
 import java.util.List;
-import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.PATCH;
