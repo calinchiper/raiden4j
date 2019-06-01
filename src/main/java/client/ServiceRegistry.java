68f56792ba293c8a202acb690b938ef7e92627f0
Calin Chiper <CalinC@yonder.local>
2019-06-01 07:45:39 +0300

    service registry

--- src/main/java/client/ServiceRegistry.java
+++ src/main/java/client/ServiceRegistry.java
@@ -11,26 +11,31 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 final class ServiceRegistry {
 
+  private static final Retrofit.Builder RETROFIT_BUILDER = initializeRetrofitBuilder();
+
   private ServiceRegistry() {
   }
 
-  static RaidenService raidenService(String baseUrl) {
-
+  private static Retrofit.Builder initializeRetrofitBuilder() {
     Gson gson = new GsonBuilder()
         .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
         .create();
 
     OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
-        .connectTimeout(30, TimeUnit.MINUTES)
-        .readTimeout(30, TimeUnit.MINUTES)
-        .writeTimeout(30, TimeUnit.MINUTES)
+        .connectTimeout(2, TimeUnit.HOURS)
+        .readTimeout(2, TimeUnit.HOURS)
+        .writeTimeout(2, TimeUnit.HOURS)
         .build();
 
     return new Retrofit.Builder()
-        .baseUrl(baseUrl)
         .addConverterFactory(GsonConverterFactory.create(gson))
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-        .client(okHttpClient)
+        .client(okHttpClient);
+  }
+
+  static RaidenService raidenService(String baseUrl) {
+    return RETROFIT_BUILDER
+        .baseUrl(baseUrl)
         .build()
         .create(RaidenService.class);
   }
